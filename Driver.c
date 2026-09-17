677692ba61f0a0dc0072f0ec01c431d7b5e94199
IanWorldHi <ianchensmiley@gmail.com>
2026-09-17 17:50:22 -0400

    Hello World v0

--- Driver.c
+++ Driver.c
@@ -1,6 +1,22 @@
-#include <ntddk.h> 
-#include <wdf.h>
-// ^^ included with "Additional IncludezDirectors"
+#include <ntddk.h> //core windows kernel definitions for all drivers
+#include <wdf.h>   //definitions for Windows Driver Framework (WDF) drivers
+// ^^ included with "Additional Include Directors"
+
+DRIVER_INITIALIZE DriverEntry;
+EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
+
+NTSTATUS
+DriverEntry( 
+	_In_ PDRIVER_OBJECT DriverObject,
+	_In_ PUNICODE_STRING RegistryPath
+)
+{ 
+	NTSTATUS status = STATUS_SUCCESS;
+
+}
+
+
+
 
 
 
